2bddb1182fd1e9914fe2f1fd46db3b381d36df6d
Niclas Nilsson <niclas.nilsson@factor10.com>
2009-08-29 14:57:11 +0200

    Made it a gem

---
+++ ruby/json2objects/lib/json2objects.rb
@@ -0,0 +1,43 @@
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+require 'json'
+require 'facets'
+
+class Json2Objects
+  VERSION = '0.0.1'
+
+  def create(json)
+    data = JSON.parse(json)
+    create_object data
+  end
+  
+  private
+  
+  def create_object(data)
+    type_name = data['Type']
+    o = eval "#{type_name}.new"
+    
+    data.each do |key, value|
+      next if key == "Type"
+
+      if value.class == Hash
+        raise "Hashes not supported yet"
+      elsif value.class == Array
+        eval "o.#{key.snakecase} = create_array value"
+      else
+        eval "o.#{key.snakecase} = #{value.inspect}"  
+      end
+    end
+    
+    o
+  end
+
+  def create_array(data)
+    arr = []
+    data.each do |k|
+      arr << create_object(k)
+    end
+    arr
+  end
+end
